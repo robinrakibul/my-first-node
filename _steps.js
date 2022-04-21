aa56d2099f0ec7df07c983fa551089759494dcc6
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-21 11:16:55 +0600

    my first node

---
+++ _steps.js
@@ -0,0 +1,17 @@
+/**
+ * 1. create folder manually or mkdir
+ * 2. open cmd to that folder
+ * 3. npm init
+ * 4. npm install express
+ * 5. create index.js
+ -------------------------------- 
+ * FOR EXPRESS / NODE SERVER
+ * 6. require express
+ * 7. create app variable
+ * 8. declare port
+ * 9. set app.get('/')
+ * 10. listen to port
+ * 11. node index.js
+ * 12. check localhost:port in browser
+ * 12. install npm nodemon to monitor live
+ */
